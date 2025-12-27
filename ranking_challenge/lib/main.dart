effa4118dc5680b7a53f28963bf420b32190dc21
Shotaro Yamazaki <shotaro.ibramessi@gmail.com>
2025-12-27 21:43:11 +0900

    feat: Initialize Flutter bindings and set preferred device orientations

--- ranking_challenge/lib/main.dart
+++ ranking_challenge/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(
     const ProviderScope(
       child: RankingChallengeApp(),
